c9ae03d09868e4335fc1037fd0d229fb15e46394
theagileengineer <sahuaditya083@gmail.com>
2026-01-13 22:43:57 +0530

    error fix for take.ts

--- src/take.ts
+++ src/take.ts
@@ -3,20 +3,10 @@ import { Operator } from "./types";
 export const take =
   <T>(limit: number): Operator<T, T> =>
     async function* (iterable) {
-      const iterator =
-        (iterable as AsyncIterable<T>)[Symbol.asyncIterator]?.() ??
-        (iterable as Iterable<T>)[Symbol.iterator]();
-
       let taken = 0;
 
-      try {
-        while (taken < limit) {
-          const { value, done } = await iterator.next();
-          if (done) return;
-          taken++;
-          yield value;
-        }
-      } finally {
-        await iterator.return?.();
+      for await (const item of iterable) {
+        if (taken++ >= limit) return;
+        yield item;
       }
     };
